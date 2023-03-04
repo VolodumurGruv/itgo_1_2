6c536ffefdfe232624a34fc0541e84243f2cf0d6
VolodumurGrushka <gru.v@i.ua>
2023-03-04 02:31:27 +0200

    update

---
+++ itgo_1_1.py
@@ -0,0 +1,98 @@
+import pygame
+from random import randint
+from pygame.constants import QUIT, K_DOWN, K_UP, K_RIGHT, K_LEFT
+
+White = (255, 255, 255)
+Black = (0, 0, 0)
+Red = (255, 0, 0)
+Green = (5, 220, 5)
+
+FPS = pygame.time.Clock()
+
+pygame.init()
+
+screen = width, height = 800, 600
+
+main_surface = pygame.display.set_mode(screen)
+
+ball = pygame.Surface((20, 20))
+ball.fill(White)
+ball_rect = ball.get_rect()
+ball_speed = 3
+
+
+
+is_working = True
+
+def create_enemy():
+    enemy = pygame.Surface((20, 20))
+    enemy.fill(Red)
+    enemy_rect = pygame.Rect(width, randint(0, height), *enemy.get_size())
+
+    return [enemy, enemy_rect, randint(2, 5)]
+
+CREATE_ENEMY = pygame.USEREVENT + 1
+pygame.time.set_timer(CREATE_ENEMY, 1500)
+
+enemies = []
+
+def create_bonus():
+  bonus = pygame.Surface((20, 20))
+  bonus.fill(Green)
+  bonus_rect = pygame.Rect(randint(0, width), 0, *bonus.get_size())
+  
+  return [bonus, bonus_rect, randint(2, 5)]
+
+CREATE_BONUS = pygame.USEREVENT + 1
+pygame.time.set_timer(CREATE_BONUS, 2000)
+
+bonuses = []
+
+while(is_working):
+    FPS.tick(130)
+
+    for event in pygame.event.get():
+        if event.type == QUIT: 
+            is_working= False
+
+        if event.type == CREATE_ENEMY:
+            enemies.append(create_enemy())
+            
+        if event.type == CREATE_BONUS: 
+          bonuses.append(create_bonus())
+
+    pressed_keys = pygame.key.get_pressed()
+    
+    main_surface.fill(Black)
+    main_surface.blit(ball, ball_rect)
+    
+    for bonus in bonuses: 
+      bonus[1] = bonus[1].move(0, bonus[2])
+      main_surface.blit(bonus[0], bonus[1])
+      
+      if bonus[1].bottom > height:
+          bonuses.pop(bonuses.index(bonus))
+
+    for enemy in enemies:
+        enemy[1] = enemy[1].move(-enemy[2], 0)
+        main_surface.blit(enemy[0], enemy[1])
+
+        if enemy[1].left < 0: 
+            enemies.pop(enemies.index(enemy))
+
+        if ball_rect.colliderect(enemy[1]):
+            enemies.pop(enemies.index(enemy))
+
+    if pressed_keys[K_DOWN] and ball_rect.bottom <= height: 
+       ball_rect = ball_rect.move([0, ball_speed])
+    
+    if pressed_keys[K_UP] and ball_rect.top > 0: 
+       ball_rect = ball_rect.move([0, -ball_speed])
+
+    if pressed_keys[K_RIGHT] and ball_rect.right <= width: 
+       ball_rect = ball_rect.move([ball_speed, 0])
+    
+    if pressed_keys[K_LEFT] and ball_rect.left > 0: 
+       ball_rect = ball_rect.move([-ball_speed, 0])
+
+    pygame.display.flip()
